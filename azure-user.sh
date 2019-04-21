f32f054417fbaa3ea274a238ed756063b0ef4710
Matthew Schwartz <Mschw90@gmail.com>
2019-04-20 21:35:21 -0500

    script 2 finished 1 almost dont and 3 admin auth works

--- azure-user.sh
+++ azure-user.sh
@@ -2,47 +2,61 @@
 
 username=$1
 password=$2
-whichFunction=$3
 
-# login to the admin
+ # login to the admin
 az login -u $username -p $password
 
-# if $whichFunction is create it will run this conditional 
-if [ $whichFunction = "create" ]; then
-    userdisplayname=$4
-    DOMAIN=mschw90gmail.onmicrosoft.com
-    userprincipalname=$userdisplayname@$DOMAIN
-    random=Thereisnopassword123!
-    usersubscription=$5
+admin=$(az role assignment list \
+    --include-classic-administrators \
+    --query "[?id=='NA(classic admins)'].principalName" | grep -E $username)
 
-    # creates a new user if the user doesnt already exist 
-    result=$(az ad user list --query [].userPrincipalName | grep -E /$userdisplayname/)
+echo $admin
 
-    if [ -z result ]; then
-        az ad user create \
-        --display-name $userdisplayname \
-        --user-principal-name $userprincipalname \
-        --force-change-password-next-login \
-        --password $random \
-        --subscription $usersubscription
+if ! [ -z $admin ]; then 
+    echo "im here"
+
+    whichFunction=$3
+    
+
+    # if $whichFunction is create it will run this conditional 
+    if [ $whichFunction = "create" ]; then
+        echo "im going to create a user"
+        userdisplayname=$4
+        DOMAIN=mschw90gmail.onmicrosoft.com
+        userprincipalname=$userdisplayname@$DOMAIN
+        random=Thereisnopassword123!
+        usersubscription=$5
+
+        # creates a new user if the user doesnt already exist 
+        result=$(az ad user list --query [].userPrincipalName | grep -E /$userdisplayname/)
+
+        if [ -n $result ]; then
+            az ad user create \
+            --display-name $userdisplayname \
+            --user-principal-name $userprincipalname \
+            --force-change-password-next-login \
+            --password $random \
+            --subscription $usersubscription
+        fi
     fi
-fi
 
-if [ $whichFunction = "assign" ]; then
-    echo "what am I"
-fi
+    # if [ $(whichFunction) = "assign" ]; then
+    #     echo "what am I"
+    # fi
 
-if [ $whichFunction = "delete" ]; then
-    echo "bye"
-fi
+    # if [ $(whichFunction) = "delete" ]; then
+    #     echo "bye"
+    # fi
 
+else 
+
+    echo "you must be an admin to access this file"
+
+    exit 1
+
+fi
 
 exit 0
 
 
 
-
-cutePuppies()
-{
-    
-}
--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ -n $(which git) ]; then 
-    ./test1.sh
-else 
-    echo "sad sad"
-fi
+username=$1
+
+admin=$(az role assignment list \
+    --include-classic-administrators \
+    --query "[?id=='NA(classic admins)'].principalName" | grep -E $username)
+
+echo $admin
 
 exit 0
