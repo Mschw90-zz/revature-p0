9dd1224d066568ffb42f02e960c7f3a7d98834a0
Matthew Schwartz <Mschw90@gmail.com>
2019-04-23 14:40:06 -0500

    added comment to linux-setup

--- linux-setup.sh
+++ linux-setup.sh
@@ -8,6 +8,8 @@ sudo apt-get install -y build-essential curl file git
     
 ## Installing Brew
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+# to add Homebrew to your PATH and to your bash shell profile script, ~/.profile on Debian/Ubuntu 
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
